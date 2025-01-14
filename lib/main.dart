bacb2ec0887d0b86186bd3d5d903aea663ee72a4
Muhammed-Jasir <jasirmm307@gmail.com>
2025-01-14 20:53:14 +0530

    Add Firebase config files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:money_tracker_app/app.dart';
+import 'package:money_tracker_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  // Add Widgets Binding
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Run the MyApp
   runApp(const MyApp());
 }
-
