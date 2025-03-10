07c98a659c829656994f22728144365847d49115
Muhammed-Jasir <jasirmm307@gmail.com>
2025-03-10 23:06:03 +0530

    Add main.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:money_tracker_app/app.dart';
-import 'package:money_tracker_app/simple_bloc_observer.dart';
 
 Future<void> main() async {
   // Add Widgets Binding
   WidgetsFlutterBinding.ensureInitialized();
-
-  Bloc.observer = SimpleBlocObserver();
 
   // Run the MyApp
   runApp(const MyApp());
